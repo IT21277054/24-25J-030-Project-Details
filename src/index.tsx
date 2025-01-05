3af3683ce13fcba55b2147b1076ff6a806f8a28a
Shamry Shiraz <IT21277054@my.sliit.lk>
2025-01-05 22:28:21 +0530

    git changes

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import Router from "./router";
 import i18n from "./translation";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/24-25J-030-Project-Details">
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
