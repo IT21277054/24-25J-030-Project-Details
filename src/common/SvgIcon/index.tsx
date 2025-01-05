bece1f3119ca9c2272a64b646e8f9d6812d757ba
Shamry Shiraz <IT21277054@my.sliit.lk>
2025-01-05 22:34:38 +0530

    changed image url

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -2,15 +2,13 @@ import { SvgIconProps } from "../types";
 
 export const SvgIcon = ({ src, width, height }: SvgIconProps) => {
   console.log("SvgIcon src:", `/img/svg/${src}`);
-  return <img src={`/img/svg/${src}`} alt={src} width={width} height={height} />;
+  return <img src={`img/svg/${src}`} alt={src} width={width} height={height} />;
 };
 
-
 export const PngIcon = ({ src, width, height }: SvgIconProps) => (
-  <img src={`/img/icons/${src}`} alt={src} width={width} height={height} />
+  <img src={`img/icons/${src}`} alt={src} width={width} height={height} />
 );
 
 export const PngIconDoc = ({ src, width, height }: SvgIconProps) => (
-  <img src={`/img/documents/${src}`} alt={src} width={width} height={height} />
+  <img src={`img/documents/${src}`} alt={src} width={width} height={height} />
 );
-
